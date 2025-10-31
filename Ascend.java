efb98333828b9103c31ba11379afc2abeca338f1
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-10-31 22:46:50 +0200

    Ascend implementation

--- Ascend.java
+++ Ascend.java
@@ -7,7 +7,7 @@ public class Ascend {
 		int num1 = (int) (Math.random() * lim);
 		int num2 = (int) (Math.random() * lim);
 		int num3 = (int) (Math.random() * lim);
-		System.out.println(num1 + " " + num2 + " " + " " + num3);
+		System.out.println(num1 + " " + num2 + " " + num3);
 		int max = Math.max(num1, Math.max(num2, num3));	
 		int min = Math.min(num1, Math.min(num2, num3));
 		int mid = ( num1 + num2 + num3 ) - max - min;
